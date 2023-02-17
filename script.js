44b3bf6c7e6d0b586bfa52784b936aaa2ccf37cc
Arghya Pal <89515522+arghyapal15@users.noreply.github.com>
2023-02-17 14:08:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var x=document.getElementById("hands");
+var y=document.getElementById("animcon");
+function closeye()
+{
+	y.style.backgroundImage="url('images/monkey_pwd.gif')";
+	x.style.marginTop="0%";
+}
+function openeye()
+{
+	y.style.backgroundImage="url('images/monkey.gif')";
+	x.style.marginTop="110%";
+}
